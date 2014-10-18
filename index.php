f04f8208f8f44f40275dc638fde53797b0cd3b1f
amertak <kadlec.david@gmail.com>
2014-10-18 21:39:44 +0200

    Init

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+/*
+$mysqli = new mysqli("localhost", "recommender", "2^83:4*%*p3.!Y!.78~h", "recommender_db");
+if ($mysqli->connect_errno) {
+echo "Failed to connect to MySQL";
+}
+
+$mysqli->set_charset('utf8');
+$mysqli->query("SET collation_connection = utf8_czech_ci");
+
+$res = $mysqli->query("SELECT * FROM test");
+while ($row = $res->fetch_assoc())
+{
+    echo $row['val'] . "<br />";
+}*/
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>WHAT</title>
+		<style type="text/css">
+			<!--
+			body {
+				color:#FFF;
+				background-color:#000000;
+				margin:0;
+			}
+
+			#container {
+				margin-top: 20%;
+				margin-left:auto;
+				margin-right:auto;
+				text-align:center;
+				}
+			-->
+		</style>
+	</head>
+	<body>
+		<div id="container">
+			<?php
+
+            //phpinfo();
+
+            ?>
+		</div>
+	</body>
+</html>
