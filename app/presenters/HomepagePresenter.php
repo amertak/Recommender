f8726df31d9931285f92940da5eab04ea73b5577
amertak <kadlec.david@gmail.com>
2014-11-08 17:38:52 +0100

    Random quotes with proper formatting and styling

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -13,15 +13,47 @@ class HomepagePresenter extends BasePresenter
 {
     /** @var Nette\Database\Context */
     private $database;
+    private $colors;
 
     public function __construct(Nette\Database\Context $database)
     {
         $this->database = $database;
+        $this->colors = ["#b92c28", "#2b669a", "#3e8f3e", "#e38d13", "#269abc"];
     }
 
 	public function renderDefault()
 	{
-        $this->template->quotes = $this->database->table('quotes');
+        $quotes = $this->database->table('quotes')->order('RAND()')->limit(1);
+
+        foreach ($quotes as $quote)
+        {
+            $this->template->text = $this->prepareText($quote->text);
+            $this->template->quote = $quote;
+        }
 	}
+
+    private function prepareText($text)
+    {
+        $text = preg_replace('/[^&lt;]&lt;\w+&gt;/', '<br />$0', $text);
+
+        if (preg_match_all('/&lt;(\w+)&gt;/', $text, $matches))
+        {
+            $count = 0;
+            $matches = array_unique($matches[1]);
+
+            foreach ($matches as $match)
+            {
+                $text = preg_replace('/&lt;(' . $match . ')&gt;/', '&lt;<font color="' . $this->colors[$count % count($this->colors)] . '">$1</font>&gt;', $text);
+                $count++;
+            }
+        }
+
+        return $text;
+    }
+
+    public function renderTest()
+    {
+        $this->redirect('default');
+    }
 
 }
